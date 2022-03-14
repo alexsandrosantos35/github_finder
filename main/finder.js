e3b056cd8f31c73b98bbacb35ce4dcb2cadd876f
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2022-03-14 11:03:42 -0300

    mais uma tentativa

--- main/finder.js
+++ main/finder.js
@@ -1,93 +1,85 @@
-const user = {
-  login: String,
-  name: String,
-  bio: String,
-  avatar_url: String,
-  public_repos: Number,
-  followers: Number,
-  following: Number,
-  repos: Array,
+const USER = {
+    login: String,
+    name: String,
+    bio: String,
+    avatar_url: String,
+    public_repos: Number,
+    followers: Number,
+    following: Number,
+    repos: Array,
 };
-const query = document.querySelector(".query");
+
+const QUERY = document.querySelector(".query");
 
 async function getUser() {
-  getRepos();
-  const url = `https://api.github.com/users/${query.value}`;
-  const data = await fetch(url);
-  const userData = await data.json();
+    getRepos();
+    const URL = `https://api.github.com/users/${QUERY.value}`;
+    const DATA = await fetch(URL);
+    const USER_INFO = await DATA.json();
 
-  if (userData.message === "Not Found") {
-    alert("User not found");
-    return;
-  }
+    USER.login = USER_INFO.login;
+    USER.name = USER_INFO.name;
+    USER.bio = USER_INFO.bio;
+    USER.avatar_url = USER_INFO.avatar_url;
+    USER.followers = USER_INFO.followers;
+    USER.following = USER_INFO.following;
+    USER.public_repos = USER_INFO.public_repos;
 
-  user.login = userData.login;
-  user.name = userData.name;
-  user.bio = userData.bio;
-  user.avatar_url = `https://github.com/${userData.login}.png`;
-  user.public_repos = userData.public_repos;
-  user.followers = userData.followers;
-  user.following = userData.following;
-
-  getAllData();
+    getAllData();
 }
 
 async function getRepos() {
-  const url = `https://api.github.com/users/${query.value}/repos`;
-  const data = await fetch(url);
-  const repos = await data.json();
-  user.repos = repos;
+    const URL = `https://api.github.com/users/${QUERY.value}/repos`;
+    const DATA = await fetch(URL);
+    const REPOS = await DATA.json();
+    USER.repos = REPOS;
 }
 
 function getAllData() {
-  const userInfo = document.getElementById("user-info");
+    const USER_INFO = document.getElementById("user-info");
 
-  userInfo.setAttribute("style", "display: block");
-  userInfo.innerHTML = `
+    USER_INFO.setAttribute("style", "display : block");
+    USER_INFO.innerHTML = `
     <div class="user-info">
-      <div class="user-avatar">
-          <h2 class="user-name">${user.name == null ? user.name = "No username" : user.name}</h2>
-          <h3 class="user-login">${user.login}</h3>
-          <img class="avatar_img" src="${user.avatar_url}" alt="${user.login}">
-          <p class="bio">${
-            user.bio == null ? (user.bio = "No bio") : user.bio
-          }</p>
-      </div>
-      <div class="user-stats">
-          <h2>Info</h2>
-          <p>Repos: ${user.public_repos}</p>
-          <p>Followers: ${user.followers}</p>
-          <p>Following: ${user.following}</p>
-      </div>
+    <div class="user-avatar">
+    <h2 class="user-name>${USER.name == null ? USER.name = "Nome indisponível" : USER.name}</h2>
+    <h3 class="uyser-login">${USER.login}</h3>
+    <img class="avatar-img" src="${USER.avatar_url}" alt="${USER.login}">
+    <p class="bio">${USER.bio == null ? USER.bio = "Bio indisponível" : USER.bio}</p>
     </div>
-    ${user.repos.map((repo) => `<div class="repo">
-      <h3 class="repo-name">${repo.name}</h3>
-      <p class="repo-description">${
-        repo.description == null
-          ? (repo.description = "No description")
-          : repo.description
-      }</p>
-      <p class="repo-language">${repo.language == null ? repo.language = "No language found" : repo.language}</p>
-      <div class="repo-stats">
-        <span>
-          ⭐${repo.stargazers_count}
-        </span>
-        <span>
-          💬${repo.watchers_count}
-        </span>
-        <span>
-          📦${repo.forks_count}
-        </span>
-        <span>
-          📅${new Date(repo.updated_at).toLocaleDateString('pt-br')}
-        </span>
-      </div>
-      <a href="${repo.html_url}" target="_blank">
-      <span>
-      🔗${repo.html_url}
-      </span>
-      </a>
+    <div class="user-stats">
+        <p>Repos: ${USER.public_repos}</p>
+        <p>Followers: ${USER.followers}</p>
+        <p>Following: ${USER.following}</p>
+    </div>
+    ${USER.repos.map((repo) => `<div class="repo">
+    <h3 class="repo-name">${repo.name}</h3>
+    <p class="repo-description">${
+        repo.description == null 
+        ? repo.description = "Sem descrição" :
+        repo.description 
+    }</p>
+    <p class="repo-language">${repo.language == null ? repo.language : repo.language}
+    <div class="repo-stats">
+    <span>
+    ${repo.starsgazers_count}
+    </span>
+    <span>
+    ${repo.watchers_count}
+    </span>
+    <span>
+    ${repo.forks_count}
+    </span>
+    <span>
+    ${new Date(repo.updated_at).LocaleDateString("pt-br")}
+    </span>
+    </div>
+    <a href="${repo.html_url}" target="_blanck">
+    <span>
+    ${repo.html_url}
+    </span>
+    </a>
     </div>`)
-      .join("")}
-  `;
-}
+            .join("")}
+   `; 
+  }
